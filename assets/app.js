971f9327f07070d760992d1dc22ec08dff3cf283
unknown <jodimcarlson4@gmail.com>
2018-11-05 20:12:45 -0600

    rylie is watching

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,15 @@
+var config = {
+    apiKey: "AIzaSyDzwd-iRQxy5NrG1U506rIMC0dGqJfNWWY",
+    authDomain: "what-should-i-eat-d6dde.firebaseapp.com",
+    databaseURL: "https://what-should-i-eat-d6dde.firebaseio.com",
+    projectId: "what-should-i-eat-d6dde",
+    storageBucket: "what-should-i-eat-d6dde.appspot.com",
+    messagingSenderId: "222114862559"
+  };
+  firebase.initializeApp(config);
+
+
+
 $.ajax({
 url: "",
 method: "GET"
@@ -11,3 +23,4 @@ Method: "GET"
 }).then(function(response){
 
 });
+
